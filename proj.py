470ec6782635615976fe5c7beab7635012e29122
Álvaro Saldanha <92269690+alvaroqsaldanha@users.noreply.github.com>
2022-10-10 11:17:33 +0100

    Update proj.py

--- proj.py
+++ proj.py
@@ -2,6 +2,7 @@ import os, os.path
 import re
 import sys
 import time
+from urllib.parse import _NetlocResultMixinStr
 from sklearn.feature_extraction.text import CountVectorizer
 from math import log
 from nltk import sent_tokenize
@@ -10,12 +11,28 @@ from dominate import document
 from dominate.tags import *
 import matplotlib.pyplot as plt 
 import itertools
+import spacy
+from scipy import spatial
 
 inv_index = {}
 idfs = {}
 n_docs = 0
 term_count = {}
 avg_dl = 0
+
+#################### TEXT PROCESSING AND INDEXING ####################
+
+def add_noun_phrases(filebody,filename):
+  nlp = spacy.load('en_core_web_sm')
+  doc = nlp(filebody)
+  count_noun_phrases = 0
+  for noun_phrase in doc.noun_chunks:
+    if inv_index.get(noun_phrase.text) == None:
+      inv_index[noun_phrase.text] = [(filename,filebody.count(noun_phrase.text))]
+    else:
+      inv_index[noun_phrase.text].append((filename,filebody.count(noun_phrase.text)))
+    count_noun_phrases+=1
+  term_count[filename] += count_noun_phrases
 
 def idf(term):
     global n_docs 
@@ -76,11 +93,14 @@ def create_bm25_dict(sentences,d):
         bm25_dict[sentence] = term_bm25 / len(fileterms)
     return bm25_dict
 
-def index_document(filename):
+def index_document(filename,preprocessing):
     global n_docs, avg_dl
     file = open(filename,"r")
     filebody = file.read()
-    vectorizer = CountVectorizer()
+    if preprocessing == "bigram":
+        vectorizer = CountVectorizer(analyzer = "word", ngram_range=(1,2))
+    else:
+        vectorizer = CountVectorizer()
     X = vectorizer.fit_transform([filebody])
     fileterms = vectorizer.get_feature_names_out()
     avg_dl += len(fileterms)
@@ -92,14 +112,16 @@ def index_document(filename):
         elif filename not in inv_index[token]:
             inv_index[token].append((filename,matrix[vectorizer.vocabulary_[token]]))
     n_docs += 1
+    if preprocessing == "noun_phrases":
+        add_noun_phrases(filebody,filename)
     file.close()
             
-def indexing(D,args):
+def indexing(D,preprocessing):
     global n_docs, avg_dl
     start = time.time()
-    for dirName, subdirList, fileList in os.walk("." + os.sep + D):
+    for dirName, subdirList, fileList in os.walk(D):
         for name in fileList:
-            index_document(os.path.join(dirName, name))
+            index_document(os.path.join(dirName, name),preprocessing)
     end = time.time()
     avg_dl = avg_dl / n_docs
     time_passed = end-start
@@ -149,10 +171,80 @@ def ranking(d,order,I,p=8,l=1000000,model="tfidf"):
     file.close()
     return (summary,sentence_dict, full_dict)
 
+#################### RRF ####################
+
+def rrf(rank_list):
+  aggregated_rank = {}
+  for sentence in rank_list[0].keys():
+    rank = 1/(0.5 + rank_list[0][sentence])
+    for model in rank_list[1:]:
+      rank += 1/(0.5 + model[sentence])
+    aggregated_rank[sentence] = rank
+  sorted_rank = dict(sorted(aggregated_rank.items(), key=operator.itemgetter(1),reverse=True))
+  return sorted_rank 
+
+#################### MMR ####################
+
+def create_mmr_rank(original_sentences,current_sentences,lb,summary):
+  mmr_rank = {}
+  original_doc = ''.join(original_sentences)
+  current_doc = ''.join(current_sentences)
+  for sentence in current_sentences:
+    vectors = [original_doc,sentence,current_doc] 
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(vectors)
+    sentence_vector = X.toarray()[1]
+    curr_vector = X.toarray()[2]
+    doc_similarity = spatial.distance.cosine(curr_vector,sentence_vector)
+    aux = 0
+    rank = (1 - lb) * doc_similarity 
+    for summary_sentence in summary:
+      vectors = [original_doc,summary_sentence] 
+      vectorizer = CountVectorizer()
+      X = vectorizer.fit_transform(vectors)
+      summary_sentence_vector = X.toarray()[1]
+      sentence_similarity = spatial.distance.cosine(sentence_vector,summary_sentence_vector)
+      aux += sentence_similarity
+    rank -= lb * aux
+    mmr_rank[sentence] = rank
+    sorted_mmr_rank = dict(sorted(mmr_rank.items(), key=operator.itemgetter(1),reverse=True)) 
+    return sorted_mmr_rank
+
+def create_mmr_summary(original_sentences,current_sentences,lb,mmr_rank,summary,p,l):
+  most_relevant_sentence = list(mmr_rank.keys())[0]
+  if p > 0 and (len(most_relevant_sentence) <= l):
+    summary.append(most_relevant_sentence)
+    p -= 1
+    l -= len(most_relevant_sentence)
+    current_sentences.remove(most_relevant_sentence)
+    new_mmr_rank = create_mmr_rank(original_sentences,current_sentences,lb,summary)
+    return create_mmr_summary(original_sentences,current_sentences,lb,new_mmr_rank,summary,p,l) 
+  return summary 
+
+def get_mmr_summary(doc,lb):
+  file = open(doc,"r")
+  filebody = file.read()
+  filebody = re.sub(r"[!][\n]+",'!',filebody)
+  filebody = re.sub(r"[?][\n]+",'?',filebody)
+  filebody = re.sub(r"[:][\n]+",':',filebody)
+  filebody = re.sub(r"[.][\n]+",'.',filebody)
+  filebody = re.sub(r"[\n]+",'.',filebody)
+  sentences = sent_tokenize(filebody)
+  original_sentences = sentences.copy()
+  mmr_rank = create_mmr_rank(original_sentences,sentences,lb,[])
+  return create_mmr_summary(original_sentences,sentences,lb,mmr_rank,[],8,500)
+
+#################### VISUALIZE ####################
+
 def visualize(d,order,I,p=7,l=1000000,model="tfidf"):
     summary,sentence_dict,full_dict= ranking(d,order,I,p,l,model)
     file = open(d,"r")
     filebody = file.read()
+    filebody = re.sub(r"[!][\n]+",'! ',filebody)
+    filebody = re.sub(r"[?][\n]+",'? ',filebody)
+    filebody = re.sub(r"[:][\n]+",': ',filebody)
+    filebody = re.sub(r"[.][\n]+",'. ',filebody)
+    filebody = re.sub(r"[\n]+",'. ',filebody)
     sentences = sent_tokenize(filebody)
     file.close()
     if order == "relevance":
@@ -171,23 +263,12 @@ def visualize(d,order,I,p=7,l=1000000,model="tfidf"):
                 div(sentence)
         br()
         div("Sentence relevance in ascending order from lighter to darker colors.",style="font-size: 10px")
-    f = open('summary.html', 'w')
+    f = open('summary_' + d + '.html', 'w')
     f.write(doc.render())
     f.close()
+    return summary
 
-def recall(summary_sentences,ref_summary_sentences):
-    matching_relevant = 0
-    for sentence in summary_sentences:
-        if sentence in ref_summary_sentences:
-            matching_relevant += 1
-    return matching_relevant / len(ref_summary_sentences)
-
-def precision(summary_sentences,ref_summary_sentences):
-    matching_relevant = 0
-    for sentence in summary_sentences:
-        if sentence in ref_summary_sentences:
-            matching_relevant += 1
-    return matching_relevant / len(summary_sentences)
+#################### EVALUATION ####################
 
 def create_results_directory(): 
     current_working_directory = os.path.abspath(os.getcwd())
@@ -235,7 +316,6 @@ def build_precision_recall_curve(doc,ref_summary_sentences, summary_sentences, p
             precision.append(current_precision)
     build_precision_recall_graph(doc,recall, precision, p, l)
 
-
 def mean_average_precision(rank, ref_summary_sentences):
     average_precision = 0
     relevant_docs = 0
@@ -278,9 +358,40 @@ def evaluation(D,S,I,P=[6,8,10,12],L=[100,200,300,400,500,750,1000,1500]):
         ref_summary_sentences = sent_tokenize(summarybody)
         summary_size_evaluation(doc.split("\\")[-1],rank,ref_summary_sentences,P,L)
 
-docs = sys.argv[1]
-doc = sys.argv[2]
+### AUXILIARY FUNCTIONS ###
+
+def build_document_list(path):
+    evaluation_list = [] 
+    for dirName, subdirList, fileList in os.walk(path):
+            for name in fileList:
+                evaluation_list.append(os.path.join(dirName,name))
+    return evaluation_list
+
+print("Please make sure BBC News Summary is in the same directory as this script.")
+print("Directory structure should be .\BBC News Summary\\News Articles\\")
+docs = '.\BBC News Summary\\News Articles\\'
+ipt = ''
+
 indexing(docs,None)
 store_idfs()
-visualize(doc,"relevance",inv_index)
-evaluation([doc],[sys.argv[3]],inv_index)
+
+while ipt != q:
+    print("\nType and enter:")
+    print("1 - to get summaries for a specific file")
+    print("2 - to perform system evaluation")
+    print("q - to exit")
+    ipt = input('>>> ')
+    if ipt == '1':
+        file = input("Please enter the name of the file, including path:\n>>> ")
+        summary = visualize(file,"relevance",inv_index)
+        print("\nSummary for " + file + ":")
+        print('\n' + summary)
+        print('\nHTML of summary created in directory.')
+    elif ipt == '2':
+        testing_set = input("Introduce the path to the set of news you want to evaluate system performance:\n>>> ")
+        reference_set = input("Introduce the corresponding summary set for comparison:\n>>> ")
+        evaluation_list = build_document_list(testing_set)
+        reference_list = build_document_list(reference_set)
+        evaluation(evaluation_list, reference_list, inv_index)
+    elif ipt == 'q':
+        exit()
